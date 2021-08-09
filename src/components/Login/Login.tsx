493c07cb049a7ec54c51e012972e7871eeb3805d
nasadiya <9604976+nasadiya@users.noreply.github.com>
2021-08-09 18:46:39 +0100

    login details comparion issue

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,27 +3,47 @@ import LoginForm from './LoginForm';
 import './Login.css';
 
 
-function Login (){
-    const [username/*, setUsername*/] = useState("");
-    //const [error, setError] = useState("");
+type creds = {
+    email:string,
+    password:string,
+}
 
-/*    const Login = (details:any) => {
-        console.log(details);
+function Login (){
+    const adminUser = {
+        email: "log@me.in",
+        password: "please"
+    }
+
+    const [user, setUser] = useState("");
+    const [Error, setError] = useState("");
+
+    const Login = (details:creds) => {
+        console.log('Login.tsx1', details);
+
+        if ((details.email === adminUser.email) && (details.password === adminUser.password)) {
+        console.log('logged in',details, adminUser,(details.email === adminUser.email),(details.password === adminUser.password));
+        setUser(details.email);
+
+        } else {
+            console.log('details do not match', details,adminUser,(details.email === adminUser.email),(details.password === adminUser.password));
+            setError('details do not match');
+        }
 
     }
 
     const Logout = () => {
         console.log("Logout");
+        setUser("");
     }
-*/
+
     return(
         <div className="Login">
-            {(username !== "") ? (
+            {(user !== "") ? (
                 <div className="Home">
-                    <h2>Welcome, <span>{username}</span></h2>
-                    <button>Logout</button>
+                    <h2>Welcome, <span>{user}</span></h2>
+                    <button onClick={Logout}>Logout</button>
                 </div>
-            ) : <LoginForm /*Login={Login} Error={error}*//>}
+            ) : <LoginForm Login={Login} Error={Error}/>}
         </div>
     );
 }
--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,31 +1,37 @@
 import React , {useState} from 'react';
 import './LoginForm.css';
 
+interface Props {
+    Login: any;
+    Error: any;
+}
 
-function LoginForm(/*{Login, Error}*/) {
-    const [details, setDetails] = useState({username:"username", password:"password"});
+function LoginForm ({Login,Error}:Props) {
+    const [details, setDetails] = useState({user:"user", password:"password"});
 
     const submitHandler = (e:any) => {
         e.preventDefault();
-        /*Login(details);*/
+        Login(details);
     }
 
     return(
+        <div>
         <form onSubmit={submitHandler}>
             <div className="form-inner">
             <span className="logo"> </span>
                 <div className="form-group">
-                    <input type="email" name="username" id ="username"
-                    onChange={e => setDetails({...details, username: e.target.value})} value={details.username}></input>
-                    <input type="password" name="password" id ="passwords"
+                    <input type="email" name="user" id ="username"
+                    onChange={e => setDetails({...details, user: e.target.value})} value={details.user}></input>
+                    <input type="password" name="password" id ="password"
                     onChange={e => setDetails({...details, password: e.target.value})} value={details.password}></input>
                     <button type="submit" >Log In</button>
                 </div>
             </div>
+        </form>
             <div className="form-outer">
                 <label> Don't have an account? <a className="color: blue;" href="!#">Sign up</a> </label>
             </div>
-        </form>
+        </div>
     )
 }
 
